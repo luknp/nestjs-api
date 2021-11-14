9c9e6786cdd6b3bf5de8b324c39a837529884c45
luknp <luk.npl@gmail.com>
2021-11-14 12:56:38 +0100

    feat: init swgger api docs

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,12 +1,16 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsInt, IsNumber, IsOptional, IsString, Min } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UserDto {
+  @ApiProperty({ example: 1 })
   @IsNumber()
   id: number;
 
+  @ApiProperty({ example: 'John' })
   @IsString()
   name: string;
 
+  @ApiProperty({ example: 'Ode' })
   @IsString()
   lastName: string;
 }
